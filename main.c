8a9bbfd61b1e2a54390d9f7d47f7a21c991bf76d
EDUARDO FILIPPSEN BARRETO <01289211027@senacrs.edu.br>
2025-11-05 21:49:39 -0300

    floats

--- main.c
+++ main.c
@@ -7,14 +7,11 @@ int main(){
     float custoPorItens = 9.99;
     float custoTotal = itens * custoPorItens;
     char moeda = '$';
+    char reais = 'R';
 
     printf("Quantidade de itens: %d\n ", itens);
-    printf("Custo por itens %.2f %c\n ", custoPorItens, moeda);
-    printf("Custo total dos itens %.2f %c\n ", custoTotal, moeda);
-    printf("O custo total foi de %c %.2f ", moeda, custoTotal);
-
-
-
+    printf("Custo por itens %c%c %.2f\n ", reais, moeda, custoPorItens);
+    printf("Custo total dos itens %c%c %.2f ", reais, moeda, custoTotal);
 
     return 0;
 }
