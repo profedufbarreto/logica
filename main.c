c34c1d5798eb17aa98fad5ee6d58360495803c74
EDUARDO FILIPPSEN BARRETO <01289211027@senacrs.edu.br>
2025-11-05 21:46:28 -0300

    usando floats diferentes

--- main.c
+++ main.c
@@ -3,12 +3,18 @@
 
 int main(){
 
-    //maneira correta
-    int minutosPorHora = 60;
-    //incorreta
-    // int mph = 60;
+    int itens = 50;
+    float custoPorItens = 9.99;
+    float custoTotal = itens * custoPorItens;
+    char moeda = '$';
 
-    printf("Uma hora tem %d minutos.", minutosPorHora);
+    printf("Quantidade de itens: %d\n ", itens);
+    printf("Custo por itens %.2f %c\n ", custoPorItens, moeda);
+    printf("Custo total dos itens %.2f %c\n ", custoTotal, moeda);
+    printf("O custo total foi de %c %.2f ", moeda, custoTotal);
+
+
+
 
     return 0;
 }
