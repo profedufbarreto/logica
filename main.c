4806f21941805cf12599978fced60f551536ab53
EDUARDO FILIPPSEN BARRETO <01289211027@senacrs.edu.br>
2025-11-05 22:26:20 -0300

    tipos de variáveis

--- main.c
+++ main.c
@@ -5,9 +5,13 @@ int main(){
 
     int resultado1 = 2 + 3 * 4;
     int resultado2 = (2 + 3) * 4;
+    int resultado3 = (2 + 4 * (2 + 8));
+    int resultado4 = ((2 + 4) * (2 + 8));
 
     printf("\n%d", resultado1);
     printf("\n%d", resultado2);
+    printf("\n%d", resultado3);
+    printf("\n%d", resultado4);
 
     return 0;
 }
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+
+    int a = 3;
+    float b = 2.60;
+    double c = 4.873;
+    char nome = 'E';
+
+    std::cout<<a<<std::endl;
+    std::cout<<b<<std::endl;
+    std::cout<<c<<std::endl;
+    std::cout<<nome<<std::endl;
+
+    return 0;
+}
