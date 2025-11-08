04fb166c6cb8f0859e710a671f5b4e22f0cc3d06
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-11-08 15:04:36 -0300

    soma simples

---
+++ following.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+
+int main(){
+
+    int a, b, x;
+
+    std::cout<<"Digite um valor para a: "<<std::endl;
+    std::cin>>a;
+
+    std::cout<<"Digite um valor para b: "<<std::endl;
+    std::cin>>b;
+
+    x = a + b;
+
+    std::cout<<x<<std::endl;
+
+    return 0;
+}
