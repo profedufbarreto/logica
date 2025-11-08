d20e87bb08e1fd8ed24f18e66973196fea1f568a
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-11-08 15:02:20 -0300

    area, raio

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 int main(){
 
+<<<<<<< HEAD
     int resultado1 = 2 + 3 * 4;
     int resultado2 = (2 + 3) * 4;
     int resultado3 = (2 + 4 * (2 + 8));
@@ -12,6 +13,16 @@ int main(){
     printf("\n%d", resultado2);
     printf("\n%d", resultado3);
     printf("\n%d", resultado4);
+=======
+    int a, b, soma;
+
+    a = 4;
+    b = 2;
+    
+    soma = a + b;
+
+    printf("O resultado é igual a: %d\n ", soma);
+>>>>>>> 6681016 (area, raio)
 
     return 0;
 }
