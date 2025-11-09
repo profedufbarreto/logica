cdc74db7d1fdfddc31cd5c8e3ec5d50e6966bcfb
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-11-09 11:36:15 -0300

    notas e média

--- following/main.cpp
+++ following/main.cpp
@@ -4,23 +4,28 @@
 
 int main(){
 
-    int a, b, c;
+    float nota1, nota2, nota3, media;
 
-    std::cout<<"Digite um valor para a: "<<std::endl;
-    std::cin>>a;
+    std::cout<<"Digite a primeira nota: "<<std::endl;
+    std::cin>>nota1;
 
-    std::cout<<"Digite um valor para b: "<<std::endl;
-    std::cin>>b;
+    std::cout<<"Digite a segunda nota: "<<std::endl;
+    std::cin>>nota2;
 
-    std::cout<<"Digite um valor para c: "<<std::endl;
-    std::cin>>c;
+    std::cout<<"Digite a terceira nota: "<<std::endl;
+    std::cin>>nota3;
 
-    if(a > b && a > c){
-        std::cout<<a<<" é o maior número!"<<std::endl;
-    }else if(b > a && b > c){
-        std::cout<<b<<" é o maior número!"<<std::endl;
+    media = (nota1 + nota2 + nota3)/3;
+
+    if(media > 7){
+        std::cout<<std::fixed<<
+        std::setprecision(2)<<media<<"! Aprovado(a)"<<std::endl;
+    }else if(media < 7 && media >= 5){
+        std::cout<<std::fixed<<
+        std::setprecision(2)<<media<<"! Recuperação"<<std::endl;
     }else{
-        std::cout<<c<<" é o maior número!"<<std::endl;
+        std::cout<<std::fixed<<
+        std::setprecision(2)<<media<<"! Reprovado"<<std::endl;
     }
 
     return 0;
