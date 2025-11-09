dad512736a7b12f30364bd1ac4392716c78b6704
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-11-09 11:29:24 -0300

    maior número

--- following/main.cpp
+++ following/main.cpp
@@ -4,15 +4,23 @@
 
 int main(){
 
-    int num;
+    int a, b, c;
 
-    std::cout<<"Digite um número: "<<std::endl;
-    std::cin>>num;
+    std::cout<<"Digite um valor para a: "<<std::endl;
+    std::cin>>a;
 
-    if(num % 2 == 0){
-        std::cout<<"O número é par!";
+    std::cout<<"Digite um valor para b: "<<std::endl;
+    std::cin>>b;
+
+    std::cout<<"Digite um valor para c: "<<std::endl;
+    std::cin>>c;
+
+    if(a > b && a > c){
+        std::cout<<a<<" é o maior número!"<<std::endl;
+    }else if(b > a && b > c){
+        std::cout<<b<<" é o maior número!"<<std::endl;
     }else{
-        std::cout<<"O número é ímpar!";
+        std::cout<<c<<" é o maior número!"<<std::endl;
     }
 
     return 0;
