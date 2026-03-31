2e1dc40977dc21101fbb8032aaf384e61008b061
profedufbarreto <01289211027@senacrs.edu.br>
2026-03-31 20:40:38 -0300

    precedence

--- teste.cpp
+++ teste.cpp
@@ -4,9 +4,14 @@ int main(){
 
     int result1 = 4 + 4 * 2;
     int result2 = 4 / 4 * 2;
+    int result3 = (4 + 4) * 2;
+    int result4 = (4 * 3) / 4;
 
     std::cout<<result1<<std::endl;
     std::cout<<result2<<std::endl;
+    std::cout<<result3<<std::endl;
+    std::cout<<result4<<std::endl;
+
 
     return 0;
 }
