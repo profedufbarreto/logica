6e12a827690034ccbe131d85c4c27787bb33dcce
profedufbarreto <01289211027@senacrs.edu.br>
2026-03-31 20:31:52 -0300

    ++

--- teste.cpp
+++ teste.cpp
@@ -2,17 +2,11 @@
 
 int main(){
 
-    int a, b;
+    int a = 5;
+    int b = ++a;
 
-    std::cout<<"Digite um valor para a: "<<std::endl;
-    std::cin>>a;
+    std::cout<<b+b<<std::endl;
 
-    std::cout<<"Digite um valor para b: "<<std::endl;
-    std::cin>>b;
-
-    std::cout<<(a != b)<<std::endl;
-
-    
 
     return 0;
 }
