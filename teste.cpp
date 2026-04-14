768882b4cdbe7bf170c49daab2074bb590138a09
profedufbarreto <01289211027@senacrs.edu.br>
2026-04-14 11:50:10 -0300

    função com &referencia para variáveis

--- teste.cpp
+++ teste.cpp
@@ -1,24 +1,25 @@
 #include<iostream>
 #include<string>
 
-void registro(){
+void registro(std::string &nome, float &altura){
 
-    std::string nome;
-    float altura;
+    std::cout<<"Digite seu nome: ";
+    std::getline(std::cin, nome);
 
-    std::cout<<"Digite seu nome: "<<std::endl;
-    std::cin>>nome;
-
-    std::cout<<"Digite sua altura: "<<std::endl;
+    std::cout<<"Digite sua altura: ";
     std::cin>>altura;
 
+    std::cout<<"--- Cadastro realizado ---"<<std::endl;
     std::cout<<"Seja bem-vindo "<<nome<<std::endl;
     std::cout<<"Você mede "<<altura<<"m."<<std::endl;
 }
 
 int main(){
 
-    registro();
+    std::string nome;
+    float altura;
+
+    registro(nome, altura);
 
     return 0;
 }
