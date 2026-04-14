e4f61d97b18caa0e09ce0fb324f30310adb7401b
profedufbarreto <01289211027@senacrs.edu.br>
2026-04-14 17:18:26 -0300

    erros para resolver

--- teste.cpp
+++ teste.cpp
@@ -1,25 +1,47 @@
 #include<iostream>
 #include<string>
 
-void registro(std::string &nome, float &altura){
+void coletarNumeros(){
 
-    std::cout<<"Digite seu nome: ";
-    std::getline(std::cin, nome);
+    int num1, num2;
 
-    std::cout<<"Digite sua altura: ";
-    std::cin>>altura;
+    std::cout<<"Digite um valor para num1: "<<std::endl;
+    std::cin>>num1;
 
-    std::cout<<"--- Cadastro realizado ---"<<std::endl;
-    std::cout<<"Seja bem-vindo "<<nome<<std::endl;
-    std::cout<<"Você mede "<<altura<<"m."<<std::endl;
+    std::cout<<"Digite um valor para num2: "<<std::endl;
+    std::cin>>num2;
+}
+
+int somar(int &num1, int &num2){
+    return num1 + num2;
+    std::cout<<"O resultado da soma é: "<<somar<<std::endl;
+}
+
+int subt(int &num1, int &num2){
+    return num1 - num2;
+    std::cout<<"O resultado da subtração é: "<<subt<<std::endl;
 }
 
 int main(){
 
-    std::string nome;
-    float altura;
+    int opcao;
+    float resposta;
 
-    registro(nome, altura);
+    std::cout<<"Digite uma opção: "<<std::endl;
+    std::cout<<"1 - Soma"<<std::endl;
+    std::cout<<"2 - Subtração"<<std::endl;
+    std::cout<<"3 - Multiplicação"<<std::endl;
+    std::cout<<"4 - Divisão"<<std::endl;
+    std::cout<<"5 - Sair"<<std::endl;
+
+    switch(opcao){
+        case 1:
+            std::cout<<somar()<<std::endl;
+            break;
+        case 2:
+            std::cout<<subt()<<std::endl;
+            break;
+    }
 
     return 0;
 }
