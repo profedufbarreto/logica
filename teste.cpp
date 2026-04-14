71a20d3257ccd32315ce6df6eabe5c43dcfa3962
profedufbarreto <01289211027@senacrs.edu.br>
2026-04-14 09:49:35 -0300

    função com variável local

--- teste.cpp
+++ teste.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 #include<string>
 
-    std::string nome;
-
-    void saudacao(){
+    void saudacao(std::string nome){
         std::cout<<"Olá "<<nome<<"!!"<<std::endl;
     }
 
 int main(){
 
+    std::string nome;
+
     std::cout<<"Digite um nome: "<<std::endl;
     std::cin>>nome;
 
-    saudacao();
+    saudacao(nome);
 
 
     return 0;
