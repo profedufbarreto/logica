70ed060a2446d651dba6e0569c3f0bd809a9fc56
profedufbarreto <01289211027@senacrs.edu.br>
2026-04-16 10:39:12 -0300

    tuple

--- teste.cpp
+++ teste.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 #include<string>
+#include <tuple>
 
 int main(){
 
-    std::string things[3] = {"Computador", "Guitarra", "Música"};
+    std::tuple<std::string, int, float> person = {"Eduardo", 38, 1.69f};
 
-    for(std::string t : things){
-        std::cout<<"Eduardo gosta de "<<t<<std::endl;
-    }
+    std::cout<<std::get<0>(person)<<std::endl;
+    std::cout<<std::get<1>(person)<<std::endl;
+    std::cout<<std::get<2>(person)<<std::endl;
 
     return 0;
 }
