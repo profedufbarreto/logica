e28f3b2139a7a3dee644d1ee0e21184d443d07ef
profedufbarreto <01289211027@senacrs.edu.br>
2026-04-16 10:31:45 -0300

    foreach com array

--- teste.cpp
+++ teste.cpp
@@ -1,46 +1,17 @@
 #include<iostream>
 #include<string>
 
-void coletarNumeros(){
-
-    int num1, num2;
-
-    std::cout<<"Digite um valor para num1: "<<std::endl;
-    std::cin>>num1;
-
-    std::cout<<"Digite um valor para num2: "<<std::endl;
-    std::cin>>num2;
-}
-
-int somar(int &num1, int &num2){
-    return num1 + num2;
-    std::cout<<"O resultado da soma é: "<<somar<<std::endl;
-}
-
-int subt(int &num1, int &num2){
-    return num1 - num2;
-    std::cout<<"O resultado da subtração é: "<<subt<<std::endl;
-}
-
 int main(){
 
-    int opcao;
-    float resposta;
+    int myNumbers[5] = {1, 2, 3, 4, 5};
+    std::string names[3] = {"Eduardo", "Casa", "Computador"};
 
-    std::cout<<"Digite uma opção: "<<std::endl;
-    std::cout<<"1 - Soma"<<std::endl;
-    std::cout<<"2 - Subtração"<<std::endl;
-    std::cout<<"3 - Multiplicação"<<std::endl;
-    std::cout<<"4 - Divisão"<<std::endl;
-    std::cout<<"5 - Sair"<<std::endl;
+    for(int num : myNumbers){
+        std::cout<<num<<std::endl;
+    }
 
-    switch(opcao){
-        case 1:
-            std::cout<<somar()<<std::endl;
-            break;
-        case 2:
-            std::cout<<subt()<<std::endl;
-            break;
+    for(std::string n : names){
+        std::cout<<n<<std::endl;
     }
 
     return 0;
