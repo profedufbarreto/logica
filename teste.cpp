0314f0de721a2620ca51b4436b31c5259d98ed96
profedufbarreto <01289211027@senacrs.edu.br>
2026-04-16 10:44:02 -0300

    tuple

--- teste.cpp
+++ teste.cpp
@@ -4,11 +4,20 @@
 
 int main(){
 
-    std::tuple<std::string, int, float> person = {"Eduardo", 38, 1.69f};
+    std::tuple<std::string, int, float> person;
 
-    std::cout<<std::get<0>(person)<<std::endl;
-    std::cout<<std::get<1>(person)<<std::endl;
-    std::cout<<std::get<2>(person)<<std::endl;
+    std::cout<<"Digite o nome para o usuário: "<<std::endl;
+    std::cin>>std::get<0>(person);
+
+    std::cout<<"Digite a idade para o usuário: "<<std::endl;
+    std::cin>>std::get<1>(person);
+
+    std::cout<<"Digite a altura para o usuário: "<<std::endl;
+    std::cin>>std::get<2>(person);
+
+    std::cout<<"O nome do usuário é: "<<std::get<0>(person)<<std::endl;
+    std::cout<<std::get<1>(person)<<" anos."<<std::endl;
+    std::cout<<"Medindo: "<<std::get<2>(person)<<"."<<std::endl;
 
     return 0;
 }
