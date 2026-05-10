caeca76362e5f63249f844c1416448f7d69e9c80
profedufbarreto <01289211027@senacrs.edu.br>
2026-05-10 11:55:41 -0300

    OO

--- main.cpp
+++ main.cpp
@@ -1,12 +1,40 @@
 #include<iostream>
-#include"Pessoa.h"
+#include<string>
+
+class ContaBancaria{
+    private:
+    std::string titular;
+    double saldo = 1000;
+
+    public:
+    ContaBancaria(std::string nome){
+        titular = nome;
+    }
+
+    void depositar(double valor){
+        if(valor > 0)
+            saldo += valor;
+    }
+
+    void sacar(double valor){
+        if (valor > 0 && valor <= saldo)
+            saldo -= valor;
+        else
+            std::cout<<"Saldo insuficiente ou valor inválido!"<<std::endl;
+    }
+
+    void exibirSaldo(){
+        std::cout<<titular<<" -- Saldo: R$ "<<saldo<<std::endl;
+    }
+};
+
 
 int main(){
 
-    Pessoa p;
-    p.coletar();
-    p.apresentar().
-
+    ContaBancaria conta("João");
+    conta.depositar(500.0);
+    conta.sacar(100.0);
+    conta.exibirSaldo();
 
     return 0;
 }
